59b32fc925aa0b477a26541e43d718d0a11f51d0
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-09-03 21:05:17 +0300

    Add project files.

---
+++ ConsoleAppHWMath/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ConsoleAppMath
+{
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter an arithmetic expression -> ");
+                string input = Console.ReadLine();
+
+                if (input?.ToLower() == "exit")
+                    break;
+
+                try
+                {
+                    var options = ScriptOptions.Default.AddReferences(typeof(Math).Assembly).AddImports("System", "System.Math");
+
+                    var result = await CSharpScript.EvaluateAsync(input, options);
+                    Console.WriteLine($"Resolt: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
